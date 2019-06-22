d39b45b2cf9d21bb18ed551570cae43a62744c49
Your Name <you@example.com>
2019-06-22 15:29:58 +0800

    商品详情页面展示

--- meiduo_mall/meiduo_mall/apps/goods/urls.py
+++ meiduo_mall/meiduo_mall/apps/goods/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     # 商品列表界面
     url(r'^list/(?P<category_id>\d+)/(?P<page_num>\d+)/$', views.ListView.as_view()),
-    # 商品列表界面
+    # 热销商品界面
     url(r'^hot/(?P<category_id>\d+)/$', views.HotGoodsView.as_view()),
+    # 商品详情界面
+    url(r'^detail/(?P<sku_id>\d+)/$', views.DetailView.as_view()),
 
 ]
